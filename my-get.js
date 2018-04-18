80b4fdb9ef4db8ddfe7275628eff6b7342686446
woniu17 <qinglucklin@foxmail.com>
2018-04-18 19:35:37 +0800

    mod my-get.js

--- my-get.js
+++ my-get.js
@@ -55,20 +55,18 @@ page.onResourceReceived = function(response) {
     var baseUrl = response.url.split('?')[0];
     var temp = baseUrl.split('/');
     var fileName = temp[temp.length-1];
+    var dir = '/root/video/';
 
     if ('m3u8' == fileName)
     {
         console.log('received: ' + fileName
             + ' bodySize: '+ response.bodySize
             + ' stage: ' + response.stage);
-        readM3u8(response.body);
-        var dir = '/root/video/';
         fs.makeDirectory(dir);
-        var path = dir + fileName;
-        fs.write(path, response.body, 'b');
+        fs.write(dir + fileName, response.body, 'b');
+        return;
     }
 
-    var paras = response.url.split('?')[1].split('&');
     var ts_start = getQueryString(response.url, 'ts_start');
     var ts_end = getQueryString(response.url, 'ts_end');
     if (0 == ts_start && 0 == ts_end) {return;}
@@ -76,7 +74,6 @@ page.onResourceReceived = function(response) {
     console.log('received: ' + fileName + ' ts_start: ' + ts_start
         + ' ts_end: ' + ts_end + ' bodySize: '+ response.bodySize
         + ' stage: ' + response.stage);
-    var dir = '/root/video/';
     fs.makeDirectory(dir);
     var path = dir + fileName + '_._' + ts_start + '_._' + ts_end;
     fs.write(path, response.body, 'b');
@@ -98,7 +95,7 @@ page.onError = function(){
 }
 
 page.captureContent = [ /.*/ ]
-page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36';
+page.settings.userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0';
 var url = 'http://v.youku.com/v_show/id_XMzUzNDE3NDc5Ng==.html?spm=a2hww.20027244.search.5&from=y1.8-4.999'
 //var url = 'http://www.baidu.com/index.html'
 // Open the web page
